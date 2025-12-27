e7e5deb2886b28056d8667baba602259a31e2ffb
Muhanguzi Muhwezi <michealphenh10@gmail.com>
2025-12-27 23:13:06 +0300

    Refactor database import and configuration in server files

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
+import Database from 'better-sqlite3';
 
-const Database = require('better-sqlite3');
 
 dotenv.config();
 
 const db = new Database(process.env.DB_PATH);
 
-module.exports = db;
+export default db;
--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,11 @@
 import dotenv from 'dotenv';
 import app from './app.js';
-import Database from 'better-sqlite3';
-
-dotenv.config({
-    path: './.env'
-});
 
 
-const db = new Database(process.env.DB_PATH);
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+dotenv.config();
 
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-});
+    });
